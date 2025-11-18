c65030782f1f27c2f5b0a7ecae45863b8c951d45
Leigh Rochelle Sanders <leighrs@farm.farm.hpc.ucdavis.edu>
2025-11-18 11:20:49 -0800

    Make scripts executable and updated metabarcoding directory setup to create samplesheet and metadata examples.

--- setup_metabarcoding_directory.sh
+++ setup_metabarcoding_directory.sh
@@ -10,11 +10,29 @@ mkdir -p "Metabarcoding/$PROJECT"
 mkdir -p "Metabarcoding/$PROJECT/input/fastq"
 mkdir -p "Metabarcoding/$PROJECT/output/intermediates_logs_cache/singularity"
 
+# Create Example_samplesheet.txt in the input folder
+cat <<EOT > "Metabarcoding/$PROJECT/input/Example_samplesheet.txt"
+sampleID	forwardReads	reverseReads	run
+B12A1_02	/home/leighrs/DSP_12S_251102/input/fastq/B12A1_02_4_S14_L001_R1_001.fastq.gz	/home/leighrs/DSP_12S_251102/input/fastq/B12A1_02_4_S14_L001_R2_001.fastq.gz	A
+B12A2_02	/home/leighrs/DSP_12S_251102/input/fastq/B12A2_02_4_S15_L001_R1_001.fastq.gz	/home/leighrs/DSP_12S_251102/input/fastq/B12A2_02_4_S15_L001_R2_001.fastq.gz	A
+B12A3_02	/home/leighrs/DSP_12S_251102/input/fastq/B12A3_02_4_S16_L001_R1_001.fastq.gz	/home/leighrs/DSP_12S_251102/input/fastq/B12A3_02_4_S16_L001_R2_001.fastq.gz	A
+EOT
+
+# Create Example_metadata.txt in the input folder
+cat <<EOT > "Metabarcoding/$PROJECT/input/Example_metadata.txt"
+ID	Replicate	Control_Assign	Sample_or_Control	Site	Month	Year
+B12A1_02	A1	1,2,4	Sample	Browns_Island	February	2023
+B12A2_02	A2	1,2,4	Sample	Browns_Island	February	2023
+B12A3_02	A3	1,2,4	Sample	Browns_Island	February	2023
+EOT
+
 echo "Directory structure created:"
 echo "Metabarcoding/"
 echo "+-- $PROJECT/"
 echo "¦   +-- input/"
-echo "        +-- fastq/"
+echo "¦       +-- fastq/"
+echo "¦       +-- Example_samplesheet.txt"
+echo "¦       +-- Example_metadata.txt"
 echo "¦   +-- output/"
-echo "        +-- intermediates_logs_cache/"
-echo "            +-- singularity/"
+echo "¦       +-- intermediates_logs_cache/"
+echo "¦           +-- singularity/"
